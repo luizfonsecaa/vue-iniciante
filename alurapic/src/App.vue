814e232a3b3d78adc9fff9f7a654a2859e09cb3f
luizfonsecaa <33426620+luizfonsecaa@users.noreply.github.com>
2018-09-14 17:37:23 -0300

    Atualização

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,12 +1,26 @@
 
 <template>
-    <div class="corpo">
-        <router-view></router-view>
-    </div>
+  <div class="corpo">
+
+   <meu-menu :rotas="routes"/>
+
+    <router-view></router-view>
+
+  </div>
 </template>
 
 <script>
+import {routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue';
 export default {
+    components:{
+        'meu-menu' : Menu
+    },
+    data(){
+        return{
+            routes,
+        }
+    }
 }
 </script>
 
